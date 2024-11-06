665da055e3e41c2b015a70a1cfcafe86c77bf03e
Thalysson <thalysson140105@gmail.com>
2024-11-06 10:52:52 -0400

    :lipstick: feat: Estilização CSS do cadastramento de despesas

--- README.md
+++ README.md
@@ -1,36 +1,38 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Introduction
 
-## Getting Started
+This is a simple project that uses the **Next.js** framework, which works with **React**, along with a Python script. The primary goal of this project is to create a web application that calculates expenses and displays a pie chart to the user. The chart shows, in percentages, what the user spent the most on and what they spent the least on.
 
-First, run the development server:
+## Step-by-Step Guide
+
+If you want to run this code using a code editor, follow these steps:
+
+* **Install** [NVM](https://github.com/nvm-sh/nvm?tab=readme-ov-file#install--update-script);
+* **Node version** -> 21.7.3 
+* Run `npm run dev` to start the website's front-end;
+* Open a new terminal;
+* Run `npm run api` (make sure you are using Node version 21.7.3);
+* Go to the port indicated by Next.js/front-end;
+* Be happy.
+
+## Tools
+
+### Python 3
+
+Install [Python](https://www.python.org/downloads/)
+
+### NVM Installation
+
+To install **NVM** (Node Version Manager), run the following command:
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 ```
+```bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+```
+### Node.js
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+```bash
+nvm use 21.7.3  # Or install it if it's not already installed
+```
--- app/page.jsx
+++ app/page.jsx
@@ -4,65 +4,64 @@ import { useState } from "react";
 export default function Home() {
 
   const [nomeDespesa, setNomeDespesa] = useState('')
-  const [valorDespesa, setvalorDespesa] = useState('')
+  const [valorDespesa, setValorDespesa] = useState('')
 
- async function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault()
-    const resposta = await fetch('http://localhost:4000/eventos',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json'
+    const resposta = await fetch('http://localhost:3051/eventos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      body:
-      JSON.stringify({
-        "titulo":nomeDespesa,
-        "valorDespesa":valorDespesa
+      body: JSON.stringify({
+        "titulo": nomeDespesa,
+        "valorDespesa": valorDespesa
       })
     })
     if (resposta.ok) {
       alert('Cadastrado com sucesso!')
-    }else{
+    } else {
       alert('ERROR!!!!!!!!!!!!')
     }
   }
 
   return (
-    <div className="m-4">
-      <h1 className="text-2xl">Cadastro de despesas</h1>
-      {/* <div className="bg-red-500 p-1">
-        Nome do evento:<b>{nomeDespesa}</b> <br />
-        Data do evento:<b>{valorDespesa}</b>
-      </div> */}
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col my-2">
-          <label htmlFor="nome">Despesa:</label>
-            <input 
-            type="text" 
-            id="nome"
-            value={nomeDespesa} 
-            onChange={e => setNomeDespesa(e.target.value)}
-            className="text-white p-1 bg-gray-500" />
-        </div>
-        <div className="flex flex-col my-2">
-          <label htmlFor="valor">Valor da despesa:</label>
-          <div className="flex items-center bg-gray-500 p-1">
-          <span className="text-white mr-2">R$</span>
-          <input
-            type="number"
-            id="valorDespesa"
-            value={valorDespesa}
-            onChange={e => setvalorDespesa(e.target.value)}
-            className="text-white bg-transparent border-none outline-none w-full"
-          />
-    </div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-sm">
+        <h1 className="text-2xl text-center text-white mb-6">Cadastro de despesas</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col my-2">
+            <label htmlFor="nome" className="text-white">Despesa:</label>
+            <input
+              type="text"
+              id="nome"
+              value={nomeDespesa}
+              onChange={e => setNomeDespesa(e.target.value)}
+              className="text-black p-2 rounded focus:outline-none"
+            />
           </div>
-          <div>
-            <input 
-            type="submit"
-            value="Cadastrar"
-            className="bg-blue-700 text-gray-100 p-1 rounded" />
+          <div className="flex flex-col my-2">
+            <label htmlFor="valor" className="text-white">Valor da despesa:</label>
+            <div className="flex items-center bg-gray-500 p-2 rounded">
+              <span className="text-white mr-2">R$</span>
+              <input
+                type="number"
+                id="valorDespesa"
+                value={valorDespesa}
+                onChange={e => setValorDespesa(e.target.value)}
+                className="text-black bg-transparent border-none outline-none w-full rounded"
+              />
+            </div>
+          </div>
+          <div className="text-center mt-4">
+            <input
+              type="submit"
+              value="Inserir"
+              className="bg-blue-700 text-gray-100 p-2 rounded cursor-pointer hover:bg-blue-600 transition"
+            />
           </div>
         </form>
       </div>
-    );
+    </div>
+  );
 }
